8cc71810743f07c535154d9a9817b1f0156f2a1d
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 17:41:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,12 @@
 # Write your methods here
 
 def loop_message_five_times(string)
-  puts 5.times string
-  
+  counter = 0
+ 
+  while counter <=5 do
+    puts string[counter]
+    counter += 1
+  end
 end
 
 
